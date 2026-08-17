6f771ab290bc04117ecfb19c020cf413058958e6
Talysson Decol <108436781+TalyssonDLF@users.noreply.github.com>
2026-08-17 00:17:16 -0300

    feat: add vehicle management and documents

---
+++ apps/api/src/vehicles/vehicles.module.ts
@@ -0,0 +1,1 @@
+import{Module}from'@nestjs/common';import{AuthModule}from'../auth/auth.module';import{PrismaModule}from'../prisma/prisma.module';import{VehiclesController}from'./vehicles.controller';import{VehiclesService}from'./vehicles.service';@Module({imports:[PrismaModule,AuthModule],controllers:[VehiclesController],providers:[VehiclesService]})export class VehiclesModule{}
